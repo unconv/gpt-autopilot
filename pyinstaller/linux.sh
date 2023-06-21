5702e09a992ae16c35737e80cd01e887c057271c
unconv <unconventionalcoding@gmail.com>
2023-06-21 18:19:14 +0300

    didn't want to have distro in the name anyway

--- pyinstaller/linux.sh
+++ pyinstaller/linux.sh
@@ -10,13 +10,10 @@ pyinstaller gpt-autopilot.py
 # add system_message to package
 cp system_message dist/gpt-autopilot/
 
-# get distro identifier
-distro=$(lsb_release -sd | tr ' ' '-')
-
 # make zip package
 cd dist/
 mkdir -p ../../zip/; 
-zip -r "../../zip/gpt-autopilot-$distro.zip" gpt-autopilot
+zip -r "../../zip/gpt-autopilot-linux.zip" gpt-autopilot
 
 # remove git repo
 cd ../../; rm -rf gpt-autopilot/
--- pyinstaller/windows.bat
+++ pyinstaller/windows.bat
@@ -10,18 +10,10 @@ pyinstaller gpt-autopilot.py
 REM add system_message to package
 copy system_message dist\gpt-autopilot\
 
-REM get distro identifier
-for /f "usebackq tokens=2 delims==" %%G in (`wmic os get Caption /value ^| findstr /r "^Caption="`) do (
-    set "distro=%%G"
-    setlocal enabledelayedexpansion
-    set "distro=%distro: =-%"
-    endlocal
-)
-
 REM make zip package using 7-Zip
 cd dist
 mkdir "..\..\zip\"
-7z a "..\..\zip\gpt-autopilot-%distro%.zip" gpt-autopilot\*
+7z a "..\..\zip\gpt-autopilot-windows.zip" gpt-autopilot\*
 
 REM remove git repo
 cd ..\..\ & rmdir /s /q gpt-autopilot
