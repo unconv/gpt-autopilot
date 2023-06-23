18ea41730745fe577353f753655d578686e918e4
unconv <unconventionalcoding@gmail.com>
2023-06-23 04:56:27 +0300

    updated betterprompter system prompt

--- betterprompter.py
+++ betterprompter.py
@@ -9,7 +9,7 @@ def make_better(prompt, model):
     messages = [
         {
             "role": "system",
-            "content": "You are a prompt designer for an AI agent that can read and write files from the filesystem and run commands on the computer. The AI agent is used to create all kinds of projects, including programming and content creaton. Please note that the agent can not run GUI applications or run tests. Only describe the project, not how it should be implemented."
+            "content": "You are a prompt designer for an AI agent that can read and write files from the filesystem and run commands on the computer. The AI agent is used to create all kinds of projects, including programming and content creaton. Please note that the agent can not run GUI applications or run tests. Only describe the project, not how it should be implemented. The prompt will be given to the AI agent as a description of the project to accomplish."
         },
         {
             "role": "user",
