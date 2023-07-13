23da06a3aaa24389ebb7cdb4ec3c5f4d3a59599f
unconv <unconventionalcoding@gmail.com>
2023-07-13 13:17:33 +0300

    fixed loop limit resetting

--- modules/chatgpt.py
+++ modules/chatgpt.py
@@ -6,7 +6,7 @@ import sys
 import os
 
 from modules.token_saver import save_tokens
-from modules.helpers import yesno, ask_input, autonomous_message_count
+from modules import helpers
 from modules import gpt_functions
 from modules import checklist
 from modules import cmd_args
@@ -62,7 +62,6 @@ def send_message(
     temp = 1.0,
 ):
     global create_outline
-    global autonomous_message_count
 
     if "loop-limit" in cmd_args.args:
         autonomous_message_limit = int(cmd_args.args["loop-limit"])
@@ -70,16 +69,16 @@ def send_message(
         autonomous_message_limit = 10
 
     # prevent function loop of death
-    autonomous_message_count += 1
-    if autonomous_message_count >= autonomous_message_limit:
-        if yesno(f"\nWARNING: ChatGPT ran {autonomous_message_count} calls back to back.\nContinue?", ["YES", "NO"]) == "NO":
+    helpers.autonomous_message_count += 1
+    if helpers.autonomous_message_count >= autonomous_message_limit:
+        if yesno(f"\nWARNING: ChatGPT ran {helpers.autonomous_message_count} calls back to back.\nContinue?", ["YES", "NO"]) == "NO":
             prompt = ask_input("\nGPT: What would you like to do next?\nYou: ")
             print()
             message = {
                 "role": "user",
                 "content": prompt,
             }
-        autonomous_message_count = 0
+        helpers.autonomous_message_count = 0
 
     # add user message to message list
     messages.append(message)
